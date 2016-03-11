592ee131af2009546b7f4d222e08406cc96ffc2b
talmeym <mtalmey@gmail.com>
2016-03-11 16:06:25 +0000

    added value source param to identify-session

--- src/main/java/com/emarte/regurgitator/core/IdentifySessionXmlLoader.java
+++ src/main/java/com/emarte/regurgitator/core/IdentifySessionXmlLoader.java
@@ -4,17 +4,18 @@ import org.dom4j.Element;
 
 import java.util.Set;
 
-import static com.emarte.regurgitator.core.CoreConfigConstants.SOURCE;
+import static com.emarte.regurgitator.core.CoreConfigConstants.*;
 import static com.emarte.regurgitator.core.Log.getLog;
 import static com.emarte.regurgitator.core.XmlConfigUtil.loadId;
 
-public class IdentifySessionXmlLoader implements XmlLoader<Step> {
+public class IdentifySessionXmlLoader extends IdentifySessionLoader implements XmlLoader<Step> {
     private static final Log log = getLog(IdentifySessionXmlLoader.class);
 
     @Override
     public Step load(Element element, Set<Object> allIds) throws RegurgitatorException {
 		String id = loadId(element, allIds);
-        log.debug("Loaded sesion identifier '" + id + "'");
-        return new IdentifySession(id, new ContextLocation(element.attributeValue(SOURCE)));
+		String source = element.attributeValue(SOURCE);
+		String value = element.attributeValue(VALUE);
+		return buildIdentifySession(id, source, value, log);
     }
 }
