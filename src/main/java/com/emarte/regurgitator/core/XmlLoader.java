50ce9917dec96da4440fe50120970f23702636ea
talmeym <mtalmey@gmail.com>
2017-09-14 13:29:23 +0100

    Refactored loaders and yml loading

--- src/main/java/com/emarte/regurgitator/core/XmlLoader.java
+++ src/main/java/com/emarte/regurgitator/core/XmlLoader.java
@@ -4,6 +4,6 @@ import org.w3c.dom.Element;
 
 import java.util.Set;
 
-public interface XmlLoader<TYPE> extends Loader {
+public interface XmlLoader<TYPE> extends Loader<Element, TYPE> {
 	TYPE load(Element element, Set<Object> allIds) throws RegurgitatorException;
 }
