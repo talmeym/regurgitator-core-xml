01fb6366fc80e20d05503519712f3d107b4c053c
talmeym <mtalmey@gmail.com>
2017-09-14 18:07:04 +0100

    Generified LoaderUtils

--- src/main/java/com/emarte/regurgitator/core/XmlLoaderUtil.java
+++ src/main/java/com/emarte/regurgitator/core/XmlLoaderUtil.java
@@ -4,22 +4,25 @@ import org.w3c.dom.Element;
 
 import static com.emarte.regurgitator.core.StringUtil.*;
 
-public class XmlLoaderUtil<TYPE extends Loader> extends LoaderUtil<TYPE> {
+public class XmlLoaderUtil<TYPE extends Loader> extends LoaderUtil<Element, TYPE> {
 
+	@Override
 	public TYPE deriveLoader(Element element) throws RegurgitatorException {
 		return buildFromClass(deriveClass(element));
 	}
 
-	private static String deriveClass(Element element) throws RegurgitatorException {
+	@Override
+	String deriveClass(Element element) throws RegurgitatorException {
 		if (element.getNamespaceURI() != null) {
 			String packageUri = stripOptionalBeginning(element.getNamespaceURI(), "http://");
-			return deriveClassName(reverseAndJoinWithDots(getMandatoryParts(packageUri, '.')), dashesToCamelCase(element.getLocalName()));
+			return deriveClass(reverseAndJoinWithDots(getMandatoryParts(packageUri, '.')), dashesToCamelCase(element.getLocalName()));
 		}
 
 		throw new RegurgitatorException("Invalid namespace: namespace missing");
 	}
 
-	private static String deriveClassName(String packageName, String className) throws RegurgitatorException {
+	@Override
+	String deriveClass(String packageName, String className) throws RegurgitatorException {
 		if (packageName == null) {
 			throw new RegurgitatorException("Package not found for class: " + className);
 		}
