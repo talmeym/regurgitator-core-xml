fe90f6dd180d8b26b08269988ffa14d7d7b1f5e4
talmeym <mtalmey@gmail.com>
2019-05-23 08:41:29 +0100

    added list processor

---
+++ src/main/java/com/emarte/regurgitator/core/ListProcessorXmlLoader.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2017 Miles Talmey.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
+package com.emarte.regurgitator.core;
+
+import org.w3c.dom.Element;
+
+import java.util.List;
+import java.util.Set;
+
+import static com.emarte.regurgitator.core.Log.getLog;
+import static com.emarte.regurgitator.core.XmlConfigUtil.loadMandatoryValueProcessors;
+
+public class ListProcessorXmlLoader implements XmlLoader<ListProcessor> {
+    private static final Log log = getLog(ListProcessorXmlLoader.class);
+
+    @Override
+    public ListProcessor load(Element element, Set<Object> allIds) throws RegurgitatorException {
+        List<ValueProcessor> processors = loadMandatoryValueProcessors(element, 0, allIds);
+        log.debug("Loaded list processor");
+        return new ListProcessor(processors);
+    }
+}
